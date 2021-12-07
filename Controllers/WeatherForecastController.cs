e913560a46a97199af7bee577438786059cd2e01
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-12-07 20:37:20 -0300

    Model View & Controller Created

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        public IActionResult SearchByCity()
+        {
+            var viewModel = new SearchByCity();
+            return View();
+        }
+
+        public IActionResult SearchByCity(SearchByCity model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherForecast", new { city = model.CityName });    
+            }
+            return View(model);
+        }
+
+        public IActionResult City(string city)
+        {
+            City viewModel = new City();
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/SearchByCity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherAPI.Models
+{
+    public class SearchByCity
+    {
+        [Required(ErrorMessage = "City name is empty!")]
+        [Display(Name = "City Name")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Invalid Input , Length must be between 2 to 20 characters")]
+        public string CityName { get; set; }
+    }
+}
